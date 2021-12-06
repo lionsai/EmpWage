a8795e917023426701fdf0c29bb4f261e0412449
lionsai <goripartisaikrishna@gmail.com>
2021-12-07 00:02:36 +0530

    Added EmpWage

--- src/EmpWage/EmpUC1.java
+++ src/EmpWage/EmpUC1.java
@@ -1,18 +1,24 @@
 package EmpWage;
 
-public class EmpUC1 {
+public class EmpUC2 {
 
 	public static void main(String[] args) {
 		
 		int is_Full_Time = 1;
-		double empCheck = Math.floor(Math.random()*10)%2;
-		
-		if(empCheck == is_Full_Time)
-			System.out.println("Employee is Present");
+		int Emp_Rate_Per_Hour = 20;
+		int Emp_Hours = 0;
+		int Emp_Wage =0 ;
+		double empCheck = Math.floor(Math.random()*10 )% 2;
+		if (empCheck == is_Full_Time)
+			
+			Emp_Hours=8;
 		else
-			System.out.println("Employee is Absent");
+			Emp_Hours=0;
+		Emp_Wage = Emp_Hours * Emp_Rate_Per_Hour;
+		System.out.println("EmpWage:" + Emp_Wage);
 		
-
+    
 	}
 
 }
+
