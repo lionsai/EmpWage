3942e62d906b5cd26ca2e88d6396c68553a63d57
lionsai <goripartisaikrishna@gmail.com>
2021-12-07 18:48:04 +0530

    Added WageCalc

--- src/EmpWage/EmpUC1.java
+++ src/EmpWage/EmpUC1.java
@@ -1,29 +1,40 @@
 package EmpWage;
 
-public class EmpUC4 {
-	
+public class EmpUC5 {
+
+
 	public  static final int is_Part_Time = 1;
 	public  static final int is_Full_Time = 2;
 	public  static final int Emp_Rate_Per_Hours = 20;
+	public static final int Number_of_Working_Days=2;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		int empHrs=0;
+		int empWage=0;
+		int totalEmpWage=0;
 		
-	    int Emp_Hours = 0;
-        int Emp_Wage = 0;
-        int emp_check=(int) Math.floor(Math.random()*10)%3;
-        switch (emp_check) {
-        
-        case is_Part_Time:
-        	Emp_Hours=4;
-        	break;
-        case is_Full_Time:
-        	Emp_Hours=8;
-        	break;
-        default :
-        	Emp_Hours=0;
-        }
-        Emp_Wage=Emp_Hours*Emp_Rate_Per_Hours;
-        System.out.println("employee wage: "+Emp_Wage);
+		for(int day=0; day<Number_of_Working_Days;day++)
+		{
+			int empCheck = (int)Math.floor(Math.random()*10)%3;
+			switch (empCheck) {
+			case is_Part_Time:
+				empHrs=4;
+				break;
+			case is_Full_Time:
+				empHrs=8;
+				break;
+			default :
+	        	empHrs=0;
+	        }
+	        empWage=empHrs*Emp_Rate_Per_Hours;
+	        totalEmpWage += empWage;
+	        System.out.println("EmpWage: "+empWage);
+         }
+         System.out.println("totalemp wage: "+totalEmpWage);
+
 		}
-       }
+	}
+
+
 
