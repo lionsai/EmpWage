14a308334e50c295c7c3d2966fb76153ce271c19
lionsai <goripartisaikrishna@gmail.com>
2021-12-07 18:36:23 +0530

    Added Part Time Employee and Wage

--- src/EmpWage/EmpUC1.java
+++ src/EmpWage/EmpUC1.java
@@ -1,24 +1,23 @@
 package EmpWage;
 
-public class EmpUC2 {
+public class EmpUC3 {
+    public static void main(String[] args) {
 
-	public static void main(String[] args) {
-		
-		int is_Full_Time = 1;
-		int Emp_Rate_Per_Hour = 20;
-		int Emp_Hours = 0;
-		int Emp_Wage =0 ;
-		double empCheck = Math.floor(Math.random()*10 )% 2;
-		if (empCheck == is_Full_Time)
-			
-			Emp_Hours=8;
-		else
-			Emp_Hours=0;
-		Emp_Wage = Emp_Hours * Emp_Rate_Per_Hour;
+        int is_Part_Time = 1;
+        int is_Full_Time = 2;
+        int Emp_Rate_Per_Hour = 20;
+        int Emp_Hours = 0;
+        int Emp_Wage = 0;
+
+        double empCheck = Math.floor(Math.random() * 10) % 3;
+        if (empCheck == is_Part_Time)
+            Emp_Hours = 4;
+        else if (empCheck == is_Full_Time)
+            Emp_Hours = 8;
+        else
+            Emp_Hours = 0;
+        Emp_Wage = Emp_Hours * Emp_Rate_Per_Hour;
 		System.out.println("EmpWage:" + Emp_Wage);
-		
-    
-	}
 
+    }
 }
-
